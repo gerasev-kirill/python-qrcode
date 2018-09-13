49a69918a5e8ccc0d7864dae7bc7520df71e8d07
Gerasev Kirill <gerasev.kirill@gmail.com>
2018-09-13 10:19:19 +0300

    [^*]

--- setup.py
+++ setup.py
@@ -4,6 +4,5 @@ from setuptools import setup
 # See setup.cfg for configuration.
 setup(
     version='6.1',
-    packages=['qrcode'],
-    data_files=[('share/man/man1', ['doc/qr.1'])],
+    packages=['qrcode']
 )
